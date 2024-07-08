e051132586d929003959a4dba2623448562331dc
Kiran Malieakkal <kiranmalieakkal18@gmail.com>
2024-07-09 00:02:29 +0200

    Added a Infowindow and customzied the pin for Advanced Marker

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import "./App.css";
 import {
   APIProvider,
@@ -9,6 +10,7 @@ import {
 
 function App() {
   const position = { lat: 53.54, lng: 10 };
+  const [open, setOpen] = useState(false);
   return (
     <APIProvider apiKey={import.meta.env.VITE_GOOGLE_MAPS_API_KEY}>
       <div className="mapContainer">
@@ -17,7 +19,28 @@ function App() {
           center={position}
           mapId={import.meta.env.VITE_GOOGLE_MAPS_ID}
         >
-          <AdvancedMarker position={position}></AdvancedMarker>
+          <AdvancedMarker
+            position={position}
+            onClick={() => {
+              setOpen(true);
+            }}
+          >
+            <Pin
+              background={"red"}
+              borderColor={"green"}
+              glyphColor={"black"}
+            />
+          </AdvancedMarker>
+          {open && (
+            <InfoWindow
+              position={position}
+              onCloseClick={() => {
+                setOpen(false);
+              }}
+            >
+              <p>I am in Hamburg</p>
+            </InfoWindow>
+          )}
         </Map>
       </div>
     </APIProvider>
